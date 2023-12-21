3d3f5323958e7d19e77f1b2ac811f5a4663c7a40
Ocyn <jcuzin@student.42lyon.fr>
2023-12-21 19:10:01 +0100

    debug.c and mem_tools.c added / Makefile rewrote / Experimental Tests in progress...

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: jcuzin <jcuzin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 13:48:44 by jcuzin            #+#    #+#             */
-/*   Updated: 2023/12/21 14:37:45 by jcuzin           ###   ########.fr       */
+/*   Updated: 2023/12/21 18:29:41 by jcuzin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	db_debug(t_linux *sys_l, void *memm1, void **memm2)
 	(void)local_array;
 	local_array = NULL;
 	printf("DB_DEBUG\n");
-	local_array = s_malloc();
+	local_array = s_malloc(sizeof(char) * );
 	return ;
 }
