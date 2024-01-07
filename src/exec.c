c86173b0b286cd715383bd4769b825716a26199b
Ocyn <niko.ocyn@gmail.com>
2024-01-07 20:46:18 +0100

    execve part started

--- src/exec.c
+++ src/exec.c
@@ -6,106 +6,82 @@
 /*   By: jcuzin <jcuzin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 14:10:31 by aammirat          #+#    #+#             */
-/*   Updated: 2024/01/07 16:00:21 by jcuzin           ###   ########.fr       */
+/*   Updated: 2024/01/07 20:45:39 by jcuzin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-void	exe_command(t_execve *cmd, pid_t *fk, char **env)
+void	exe_command(t_cmd *cmd, pid_t *fk)
 {
 	char	*path;
 
 	path = NULL;
-	(void)fk;
-	// *fk = fork();
-	// if (*fk == -1)
-	// 	return ;
-	// if (*fk == 0)
-	// {
-		printf("\n\t\t[%s]", cmd->prefixes[0]);
-		path = get_path(cmd->prefixes[0], env);
-		printf("execve(path[%s] | \n)", path);
-		db_tabstr_display(cmd->prefixes, "args", -1);
-		printf(" | Env[Too long]\n)");
-		// execve(path, cmd->args, env);
-		// perror("bash");
+	*fk = fork();
+	if (*fk == -1)
+		return ;
+	if (*fk == 0)
+	{
+		db_print_custom_font("\nExecution\n", FE_UND);
+		path = get_path(cmd->command.prefixes[0], cmd->command.env_var);
+		printf("execve(%s, ", path);
+		db_tabstr_display(cmd->command.prefixes, "args", -1);
+		printf(", Env[Too long])\n");
+		execve(path, cmd->command.prefixes, cmd->command.env_var);
+		perror("bash");
 		s_free(&path);
-		// exit (0);
-	// }
+	}
 }
 
-// void	stdfdout_dup(pid_t *fk, t_cmd *command, char **env)
-// {
-	
-// 	// dup2(command->outfile, STDOUT_FILENO);
-// 	// exe_command(&command->command, fk, env);
-// 	// close(command->outfile);
-// 	// close(STDOUT_FILENO);
-// }
-
-// void	stdfdin_dup(pid_t *fk, t_cmd *command, char **env)
-// {
-// 	// dup2(command->infile, STDIN_FILENO);
-// 	// exe_command(&command->command, fk, env);
-// 	// close(command->infile);
-// 	// close(STDIN_FILENO);
-// }
-
-void	select_dup(pid_t *fk, t_cmd *command, char **env)
+void	redirection(int pip, int pipnext)
 {
-	(void)fk;
-	(void)env;
-	printf("\n\tRedirect : ");
-	if (command->type == INFILE_CMD)
-	{
-		printf("STDIN -> ");
-	}
-	if (command->type == OUTFILE_CMD)
-	{
-		printf("STDOUT -> ");
-	}
-	if (command->type == PIPE_CMD)
-	{
-		printf("Output of [%d", command->id);
-		db_tabstr_display(command->command.prefixes, "", -1);
-	}
-	if (command->type == INFILE_CMD && command->type == OUTFILE_CMD)
-		db_tabstr_display(command->command.args, "File", -1);
-	if (command->type == PIPE_CMD && command->next)
-	{
-		db_tabstr_display(command->next->command.prefixes, "Input of Next", -1);
-	}
+	dup2(pipnext, pip);
+	close(pip);
 }
 
-// void	pipe_dup(int pipefd[2], pid_t *fk, t_cmd *command, char **env)
-// {
-// 	// close(pipefd[(command->id % 2 + 1)]);
-// 	// dup2(pipefd[command->id % 2], command->id % 2);
-// 	// exe_command(&command->command, fk, env);
-// 	// close(pipefd[command->id % 2]);
-// }
+int	select_dup(int *pip, int *pipnext, t_cmd *command)
+{
+	if (command->prev && command->prev->type == INFILE_CMD)
+		redirection(set_infile(command->command.args[0]), STDIN_FILENO);
+	if (command->next && command->next->type == OUTFILE_CMD)
+		redirection(set_outfile(command->command.args[0]), STDOUT_FILENO);
+	if (command && command->type == PIPE_CMD)
+		redirection(pipnext[0], pip[1]);
+	if (command->prev && command->prev->type == PIPE_CMD)
+		redirection(pip[0], pipnext[1]);
+	if (command && (command->type == INFILE_CMD || command->type == OUTFILE_CMD \
+	|| command->type == OUTFILE_ADDER))
+		return (0);
+	return (1);
+}
 
 void	launch_command(t_linux *shell)
 {
 	t_cmd	*command;
 	pid_t	fork_id;
-	int		pipefd[2];
+	int		pipfd[2];
+	int		pipfd_next[2];
 
 	fork_id = 0;
-	pipefd[0] = 0;
-	pipefd[1] = 0;
-	//pipe(pipefd);
-	(void)command;
-	//if (!pipefd[0] || !pipefd[1])
-		//return ;
-	command = shell->head;
+	ft_memset(pipfd, 0, 2);
+	ft_memset(pipfd_next, 0, 2);
+	pipe(pipfd);
+	pipe(pipfd_next);
+	// (void)command;
+	if (!pipfd[0] || !pipfd[1] || !pipfd_next[0] || !pipfd_next[1])
+		return ;
+	command = shell->head->next;
 	while (command)
 	{
-		select_dup(&fork_id, command, shell->envi);
-		exe_command(&command->command, &fork_id, shell->envi);
+		command->command.env_var = shell->envi;
+		if (select_dup(pipfd, pipfd_next, command))
+			exe_command(command, &fork_id);
 		command = command->next;
 	}
-	//waitpid(fork_id, 0, 0);
-	//waitpid(fork_id - 1, 0, 0);
+	waitpid(fork_id, 0, 0);
+	waitpid(fork_id - 1, 0, 0);
+	if (close(pipfd[0] == -1 || close(pipfd[1] == -1)))
+		printf("\nCLOSING FAILED\n");
+	if (close(pipfd_next[0] == -1 || close(pipfd_next[1] == -1)))
+		printf("\nCLOSING FAILED\n");
 }
