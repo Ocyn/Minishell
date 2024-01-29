6450ac0d0b00b39d25800d0b9cdf9971df46d241
nonomex <81557850+nonomex@users.noreply.github.com>
2024-01-29 17:38:39 +0100

    j'enleve du debug

--- src/signal.c
+++ src/signal.c
@@ -15,7 +15,6 @@
 void	ctrl_c(int sig, siginfo_t *inf, void *gain)
 {
 	write (1, "\n", 1);
-	printf ("hey mais je suis la en fait \n");
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
