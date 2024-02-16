c94216b5e16b9f4cbf761e196c29d0b1e41b5da1
Ocyn <niko.ocyn@gmail.com>
2024-02-16 14:49:29 +0100

    [DONT PUSH] jcuzin push currently working on parse/build_command/set_command_metadatas: working on get_redirection

--- src/build_command.c
+++ src/build_command.c
@@ -6,7 +6,7 @@
 /*   By: jcuzin <jcuzin@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 05:49:19 by jcuzin            #+#    #+#             */
-/*   Updated: 2024/02/16 13:52:46 by jcuzin           ###   ########.fr       */
+/*   Updated: 2024/02/16 14:45:08 by jcuzin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,30 +50,50 @@ int	find_str_in_list(t_lst *list_input, char *key)
 	return (-1);
 }
 
-int	get_type(char **src, char *type)
+int	get_type(t_lst *list)
 {
-	int	i;
+	t_lst	*new;
+	int		type;
 
-	i = -1;
-	if (!src || !type || i == -1)
-		return (0);
-	i = find_str_in_tab(0, type, src);
-	if (i == -1)
-		return (1);
-	// Checker si y'a au moins un char imprimable apres le type
-	// Renvoyer erreur de syntaxe si echex puis interdire la partie de l'execution et juste revenir à readline
-	return (0);
+	type = _TOK_EMPTY;
+	new = NULL;
+	if (!list)
+		return (type);
+	if (find_str_in_str((char *)list->data, "<") != -1)
+	{
+		type = _TOK_INFILE;
+		if (find_str_in_str((char *)list->data, "<<") != -1)
+			type = _TOK_HEREDOC;
+		if (ft_strlen((char *)list->data) > 1)
+			str_edit((char *)(&list->data), "<", "");
+	}
+	if (find_str_in_str((char *)list->data, ">") != -1)
+	{
+		type = _TOK_OUTFILE;
+		if (find_str_in_str((char *)list->data, ">>") != -1)
+			type = _TOK_OUTFILE_APPEND;
+		if (ft_strlen((char *)list->data) > 1)
+			str_edit((char *)(&list->data), ">", "");
+	}
+	return (type);
 }
 
-t_cmd	*get_redirection(t_cmd *cmd, t_lst *key_words)
+t_cmd	*get_redirection(t_cmd *cmd, t_lst *list)
 {
-	int		i;
+	t_lst	*head;
 
-	i = 0;
-	db_print_custom_font("\n\nGet_Redirection\n", FE_UND);
-	db_display_list(key_words, "Key_words", 's');
+	head = list;
+	db_print_custom_font("\n\nGet_Redirection\n", FE_BOL);
+	db_display_list(list, "Key_words", 's');
 	(void)cmd;
-	(void)i;
+	while (list)
+	{
+		if (list->id > 0)
+		{
+			if (get_type(list) == _TOK_INFILE)
+		}
+		list = list->next;
+	}
 	// Missing ">>" alias outfile append (no overwrite) case
 	return (cmd);
 }
