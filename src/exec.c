f880c250459a447eac261f9719621a1dec44b052
Ocyn <niko.ocyn@gmail.com>
2024-02-21 15:43:29 +0100

    wertyuio

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: jcuzin <jcuzin@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 14:10:31 by aammirat          #+#    #+#             */
-/*   Updated: 2024/02/21 15:24:25 by jcuzin           ###   ########.fr       */
+/*   Updated: 2024/02/21 15:41:14 by jcuzin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,13 @@ void	exe_command(t_cmd *cmd, pid_t *fk, int *pip, t_linux *shell)
 		return ;
 	if (*fk == 0)
 	{
+		printf("\n\tChild:\n");
+		printf("\t\tpip[0]: [%d]\n", pip[0]);
+		printf("\t\tpip[1]: [%d]\n", pip[1]);
 		path = get_path(cmd->meta.exec_cmd[0], shell->env);
 		if (path != NULL)
 		{
-			execve(path, cmd->meta.exec_cmd, shell->envi);
+			execve(path, cmd->meta.exec_cmd, shell-> envi);
 			perror("minishell");
 			exit_forkfailure(EXIT_FAILURE, shell, pip, &path);
 		}
@@ -68,7 +71,7 @@ void	launch_command(t_linux *shell, t_cmd *command)
 	command = shell->head->next;
 	while (command && command->meta.exec_cmd)
 	{
-		if (!command->next && pipe(pip) == -1)
+		if (pipe(pip) == -1)
 			break ;
 		select_dup(pip, old_pipr, command);
 		if (!is_builtin(command->meta.exec_cmd[0], shell))
@@ -77,12 +80,11 @@ void	launch_command(t_linux *shell, t_cmd *command)
 				exe_command(command, &fork_id, pip, shell);
 			waitpid(fork_id, &g_sign, 0);
 		}
-		old_pipr = dup(pip[1]);
 		s_close(pip[0]);
-		s_close(pip[1]);
-		s_close(old_pipr);
+		dup2(old_pipr, pip[1]);
 		command = command->next;
 	}
+	s_close(old_pipr);
 	change_ret_signal(g_sign);
 	create_signal();
 	/*DEBUG*/	db_printf("\n\nCOMMANDS ALL DONE\n\n", FE_BOL);
